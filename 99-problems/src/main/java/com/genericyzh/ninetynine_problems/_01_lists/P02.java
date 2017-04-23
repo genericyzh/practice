6488ba0a2c5cd5b6161f9d4ba1c3339527bfced8
genericyzh <yaozihuan@163.com>
2017-04-24 00:00:37 +0800

    【P02】自己练习重写一次

--- 99-problems/src/main/java/com/genericyzh/ninetynine_problems/_01_lists/P02.java
+++ 99-problems/src/main/java/com/genericyzh/ninetynine_problems/_01_lists/P02.java
@@ -4,23 +4,49 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/*
+Find the last but one element of a list
+ */
 public class P02 {
 
+//    public static <T> T secondLast(List<T> list) {
+//        if (list.size() < 2) {
+//            throw new NoSuchElementException("Can't find secondLast element from a list with less than 2 elements");
+//        }
+//        return list.get(list.size() - 2);
+//    }
+
     public static <T> T secondLast(List<T> list) {
+        if (list == null) {
+            throw new NullPointerException("list为空");
+        }
         if (list.size() < 2) {
-            throw new NoSuchElementException("Can't find secondLast element from a list with less than 2 elements");
+            throw new NoSuchElementException("list长度小于2，不能找出倒数第二个元素");
         }
         return list.get(list.size() - 2);
     }
 
+//    public static <T> T secondLastRecursion(LinkedList<T> list) {
+//        if (list.size() < 2) {
+//            throw new NoSuchElementException("Can't find secondLast element from a list with less than 2 elements");
+//        }
+//        if (list.size() == 2) {
+//            return list.getFirst();
+//        }
+//        return secondLastRecursion(new LinkedList<>(list.subList(1, list.size())));
+//
+//    }
+
     public static <T> T secondLastRecursion(LinkedList<T> list) {
+        if (list == null) {
+            throw new NullPointerException("list为空");
+        }
         if (list.size() < 2) {
-            throw new NoSuchElementException("Can't find secondLast element from a list with less than 2 elements");
+            throw new NoSuchElementException("list长度小于2，不能找出倒数第二个元素");
         }
         if (list.size() == 2) {
             return list.getFirst();
         }
-        return secondLastRecursion(new LinkedList<>(list.subList(1, list.size())));
-
+        return secondLastRecursion((new LinkedList<T>(list.subList(1, list.size()))));
     }
 }
