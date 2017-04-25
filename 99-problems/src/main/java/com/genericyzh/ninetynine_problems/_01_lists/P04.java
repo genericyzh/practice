31f315ad78b6d293d8eb0cadbab43ce8948a9d71
genericyzh <yaozihuan@163.com>
2017-04-25 23:54:46 +0800

    【P0】返回一个List的元素个数

--- 99-problems/src/main/java/com/genericyzh/ninetynine_problems/_01_lists/P04.java
+++ 99-problems/src/main/java/com/genericyzh/ninetynine_problems/_01_lists/P04.java
@@ -4,10 +4,25 @@ import java.util.List;
 
 /**
  * Find the number of elements of a list
+ * 返回一个List的元素个数
  */
 public class P04 {
 
+//    public static <T> int length(List<T> list) {
+//        return list.size();
+//    }
+
+    /**
+     * 直接调用API
+     *
+     * @param list
+     * @param <T>
+     * @return
+     */
     public static <T> int length(List<T> list) {
+        if (list == null) {
+            throw new NullPointerException();
+        }
         return list.size();
     }
 
@@ -19,14 +34,36 @@ public class P04 {
         return list.stream().mapToInt(x -> 1).sum();
     }
 
+//    public static <T> int lengthRecursive(List<T> list) {
+//        return _lengthRecursive(list, 0);
+//    }
+//
+////    private static <T> int _lengthRecursive(List<T> list, int i) {
+////        if (list.isEmpty()) {
+////            return i;
+////        }
+////        return _lengthRecursive(list.subList(1, list.size()), ++i);
+////    }
+
     public static <T> int lengthRecursive(List<T> list) {
         return _lengthRecursive(list, 0);
     }
 
-    private static <T> int _lengthRecursive(List<T> list, int i) {
+    /**
+     * 递归写法;感觉好蠢..
+     *
+     * @param list
+     * @param <T>
+     * @return
+     */
+    public static <T> int _lengthRecursive(List<T> list, int i) {
+        if (list == null) {
+            throw new NullPointerException();
+        }
         if (list.isEmpty()) {
             return i;
         }
         return _lengthRecursive(list.subList(1, list.size()), ++i);
     }
+
 }
