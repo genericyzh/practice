14f82e646cf7cec4f0fb0ca7f0d0b09f53dd9eb5
genericyzh <yaozihuan@163.com>
2017-05-24 23:11:14 +0800

    【leetcode】简单题目fizz-buzz

--- 99-problems/src/main/java/com/genericyzh/algoDS/arrays/_01_RotateArray.java
+++ 99-problems/src/main/java/com/genericyzh/algoDS/arrays/_01_RotateArray.java
@@ -69,4 +69,17 @@ public class _01_RotateArray {
         }
     }
 
+    public static void main(String args[]) {
+        String Str1 = new String("runoob");
+        String Str2 = Str1;
+        String Str3 = new String("runoob");
+        boolean retVal;
+
+        retVal = Str1.equals( Str2 );
+        System.out.println("返回值 = " + retVal );
+
+        retVal = Str1.equals( Str3 );
+        System.out.println("返回值 = " + retVal );
+    }
+
 }
