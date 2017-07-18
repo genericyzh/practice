46ce0510cc039ccc2b942e4118ddee4997b4f1da
genericyzh <yaozihuan@163.com>
2017-07-18 22:26:22 +0800

    实现单向链表

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/linkedlist/_01_LinkedList.java
@@ -0,0 +1,71 @@
+package com.genericyzh.algoDS.linkedlist;
+
+import java.util.NoSuchElementException;
+
+/**
+ * 实现单向链表
+ *
+ * @author genericyzh
+ * @date 2017/7/18 21:43
+ */
+public class _01_LinkedList<E> {
+    int size;
+    private Node head;
+    private Node tail;
+
+    public void add(E element) {
+        // 如果链表是空的时候，头跟尾是一样的
+        if (head == null) {
+            head = tail = new Node(element);
+        } else {
+            tail.next = new Node(element);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new NoSuchElementException();
+        }
+
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node.element;
+    }
+
+    public boolean delete(E element) {
+        Node temp = new Node(null, head);
+        Node dummy = temp;
+        while (temp.next != null) {
+            if (temp.next.element.equals(element)) {
+                if (temp.next == tail) {
+                    tail = temp;
+                }
+                temp.next = temp.next.next;
+                // 优雅的处理head的element等于element的情况，包括只有一个元素的情况
+                head = dummy.next;
+                size--;
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    private class Node {
+        E element;
+        Node next;
+
+        public Node(E e) {
+            this.element = e;
+        }
+
+        public Node(E e, Node next) {
+            this.element = e;
+            this.next = next;
+        }
+    }
+}
