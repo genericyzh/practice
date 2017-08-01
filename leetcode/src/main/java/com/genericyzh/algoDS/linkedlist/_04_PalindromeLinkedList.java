9aaa67c82137c4ebbc99e0d72af9898f36c26277
genericyzh <yaozihuan@163.com>
2017-08-01 23:32:04 +0800

    234. Palindrome Linked List 判断回文单向链表

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/linkedlist/_04_PalindromeLinkedList.java
@@ -0,0 +1,76 @@
+package com.genericyzh.algoDS.linkedlist;
+
+/**
+ * https://leetcode.com/problems/palindrome-linked-list/description/
+ * 给一个单向链表，判断是否是回文，最好是O(n) time and O(1) space
+ * 思路：先得到链表长度，再利用递归的特性:栈中存head的next，全局存rightNode，从中间向两边判断
+ *
+ * @author genericyzh
+ * @date 2017/8/1 22:46
+ */
+public class _04_PalindromeLinkedList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    ListNode rightStart = null;
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode temp = head;
+        rightStart = head;
+        int size = 0;
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+
+        int start = (size - 1) / 2 + 1;
+        for (int i = 0; i < start; i++) {
+            rightStart = rightStart.next;
+        }
+
+        return checkTwoHalves(head, size / 2);
+
+    }
+
+    private boolean checkTwoHalves(ListNode x, int size) {
+        if (size == 1) {
+            return x.val == rightStart.val;
+        }
+        boolean b = checkTwoHalves(x.next, size - 1);
+        rightStart = rightStart.next;
+        return b && x.val == rightStart.val;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(2);
+        ListNode listNode4 = new ListNode(1);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+
+        _04_PalindromeLinkedList palindromeLinkedList = new _04_PalindromeLinkedList();
+        System.out.println(palindromeLinkedList.isPalindrome(listNode1));
+
+    }
+}
