c55fe03eee894457d0c68fbebb46cead060e5f81
genericyzh <yaozihuan@163.com>
2017-08-21 13:02:06 +0800

    【修改参数名】

--- leetcode/src/main/java/com/genericyzh/algoDS/arrays/_05_KthLargestElementinanArray.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/arrays/_05_KthLargestElementinanArray.java
@@ -17,25 +17,25 @@ public class _05_KthLargestElementinanArray {
         return partition(nums, 0, nums.length - 1, nums.length - k);
     }
 
-    int partition(int[] a, int lo, int hi, int k) {
-        int p = (lo + hi) / 2; // 选择pivot
-        int curIndex = lo;
+    int partition(int[] a, int left, int right, int k) {
+        int p = (left + right) / 2; // 选择pivot
+        int curIndex = left;
 
         // 把小于pivot的元素从左堆到右
-        swap(a, p, hi); // 先弄到最后
-        for (int i = lo; i < hi; i++) {
-            if (a[i] < a[hi]) {
+        swap(a, p, right); // 先弄到最后
+        for (int i = left; i < right; i++) {
+            if (a[i] < a[right]) {
                 swap(a, i, curIndex++);
             }
         }
-        swap(a, curIndex, hi); // 记得还原
+        swap(a, curIndex, right); // 记得还原
 
         if (curIndex == k) {
             return a[k];
         } else if (curIndex < k) { // k在右边，继续筛选
-            return partition(a, curIndex + 1, hi, k);
+            return partition(a, curIndex + 1, right, k);
         } else { // k在左边，继续筛选
-            return partition(a, lo, curIndex - 1, k);
+            return partition(a, left, curIndex - 1, k);
         }
     }
 
