fab923db7361676e3ffc44a35cc07f683a0b73f1
genericyzh <yaozihuan@163.com>
2017-08-28 23:47:33 +0800

    102. Binary Tree Level Order Traversal 广搜二叉树

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_01_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,49 @@
+package com.genericyzh.algoDS.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+ * 给出一个二叉树，依次输出每一层的节点
+ *
+ * @author genericyzh
+ * @date 2017/8/28 23:37
+ */
+public class _01_BinaryTreeLevelOrderTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result; // 这里应该返回[]，而不是null
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
