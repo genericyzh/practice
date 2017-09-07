d711de3c1e19be41899f48b8883006028aecd76c
genericyzh <yaozihuan@163.com>
2017-09-07 23:18:31 +0800

    104. Maximum Depth of Binary Tree 找出一个二叉树最大深度

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_11_MaximumDepthofBinaryTree.java
@@ -0,0 +1,18 @@
+package com.genericyzh.algoDS.binarytree;
+
+/**
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ * 给出一个二叉树，求出最大深度
+ *
+ * @author genericyzh
+ * @date 2017/9/7 23:13
+ */
+public class _11_MaximumDepthofBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
