c525cd6118f63c1eabb0e80a5e5b9dc71aea1674
genericyzh <yaozihuan@163.com>
2017-09-16 23:56:05 +0800

    450. Delete Node in a BST

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_19_DeleteNodeinaBST.java
@@ -0,0 +1,11 @@
+package com.genericyzh.algoDS.binarytree;
+
+/**
+ * https://leetcode.com/problems/delete-node-in-a-bst/description/
+ * 给出一个二叉搜搜树，删除一个节点
+ *
+ * @author genericyzh
+ * @date 2017/9/16 23:55
+ */
+public class _19_DeleteNodeinaBST {
+}
