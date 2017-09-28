359993b9bd1416c18ba83c057c028c6071e2a0d3
genericyzh <yaozihuan@163.com>
2017-09-28 21:42:01 +0800

    【希尔排序】

--- leetcode/src/main/java/com/genericyzh/algoDS/algo/sortingandsearching/_02_InsertSort.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/algo/sortingandsearching/_02_InsertSort.java
@@ -4,6 +4,8 @@ package com.genericyzh.algoDS.algo.sortingandsearching;
  * 插入排序：参考图：http://images.cnitblog.com/i/497634/201403/121755260214693.jpg
  * 最佳情况就是，数组已经是正序排列了，在这种情况下，需要进行的比较操作是 （n-1）次即可。
  * 最坏情况就是，数组是反序排列，那么此时需要进行的比较共有n(n-1)/2次
+ * <p>
+ * https://www.kancloud.cn/kancloud/data-structure-and-algorithm-notes/72913
  *
  * @author genericyzh
  * @date 2017/9/26 23:43
@@ -60,6 +62,27 @@ public class _02_InsertSort {
         }
 
         return helper.next;
+    }
+
+    /**
+     * 希尔排序:https://zh.wikipedia.org/wiki/%E5%B8%8C%E5%B0%94%E6%8E%92%E5%BA%8F
+     * 插入排序的改进
+     * 注意：步长的选择有很多种
+     *
+     * @param arr
+     */
+    public static void shell_sort(int[] arr) {
+        int gap = 1, i, j, len = arr.length;
+        int temp;
+        while (gap < len / 3)
+            gap = gap * 3 + 1; // <O(n^(3/2)) by Knuth,1973>: 1, 4, 13, 40, 121, ...
+        for (; gap > 0; gap /= 3)
+            for (i = gap; i < len; i++) {
+                temp = arr[i];
+                for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+                    arr[j + gap] = arr[j];
+                arr[j + gap] = temp;
+            }
     }
 
     public static void main(String[] args) {
