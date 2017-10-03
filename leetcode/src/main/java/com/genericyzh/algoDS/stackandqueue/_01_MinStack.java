99554b92375c4a1abaa6c56e540758dc54cf137d
genericyzh <yaozihuan@163.com>
2017-10-03 23:06:12 +0800

    【包含 min 函数的栈】

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/stackandqueue/_01_MinStack.java
@@ -0,0 +1,50 @@
+package com.genericyzh.algoDS.stackandqueue;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * @author genericyzh
+ * @date 2017/10/3 22:56
+ */
+
+/**
+ * 省了第二个栈的一点空间
+ */
+public class _01_MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> min; // 辅助栈是一个记录当前栈最小值的栈，是一个降序序列
+
+    /**
+     * initialize your data structure here.
+     */
+
+    public _01_MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        // isEmpty是首次加入时，只有当小于才加入，所以是一个降序栈
+        if (min.isEmpty() || x <= min.peek())
+            min.push(x);
+    }
+
+    public void pop() {
+        int p = stack.pop();
+        // 只有最小值pop，才会更新辅助栈的栈顶
+        if (min.peek() == p)
+            min.pop();
+
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+}
