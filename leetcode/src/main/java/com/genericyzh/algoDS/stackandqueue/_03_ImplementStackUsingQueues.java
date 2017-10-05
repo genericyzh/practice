dfacfcec328b0be84afbcfcf353e0dfae35318e7
genericyzh <yaozihuan@163.com>
2017-10-05 23:27:46 +0800

    225. Implement Stack using Queues   挖坑

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/stackandqueue/_03_ImplementStackUsingQueues.java
@@ -0,0 +1,10 @@
+package com.genericyzh.algoDS.stackandqueue;
+
+/**
+ * https://leetcode.com/problems/implement-stack-using-queues/description/
+ *
+ * @author genericyzh
+ * @date 2017/10/5 23:26
+ */
+public class _03_ImplementStackUsingQueues {
+}
