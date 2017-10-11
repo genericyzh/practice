e500096f89e231186e39d0718c807b88bc94d767
genericyzh <yaozihuan@163.com>
2017-10-11 23:40:31 +0800

    8. String to Integer (atoi)  将string转换为int

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/string/_05_StringToInteger.java
@@ -0,0 +1,48 @@
+package com.genericyzh.algoDS.string;
+
+/**
+ * https://leetcode.com/problems/string-to-integer-atoi/description/
+ * 将string转换为int
+ *
+ * @author genericyzh
+ * @date 2017/10/11 23:33
+ */
+public class _05_StringToInteger {
+
+    public int myAtoi(String str) {
+        if (str == null || str.length() == 0) return 0;
+
+        // trim left and right spaces
+        String strTrim = str.trim();
+        int len = strTrim.length();
+        // sign symbol for positive and negative
+        int sign = 1;
+        // index for iteration
+        int i = 0;
+        if (strTrim.charAt(i) == '+') {
+            i++;
+        } else if (strTrim.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+
+        // store the result as long to avoid overflow
+        long result = 0;
+        while (i < len) {
+            if (strTrim.charAt(i) < '0' || strTrim.charAt(i) > '9') {
+                break;
+            }
+            result = 10 * result + sign * (strTrim.charAt(i) - '0');
+            // overflow
+            if (result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            } else if (result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+
+        return (int) result;
+    }
+
+}
