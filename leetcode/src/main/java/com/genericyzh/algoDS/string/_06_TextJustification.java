ae75febd1fab55649178099b80b0266bcc491bb1
genericyzh <yaozihuan@163.com>
2017-10-13 00:23:38 +0800

    68. Text Justification  填坑

--- leetcode/src/main/java/com/genericyzh/algoDS/string/_06_TextJustification.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/string/_06_TextJustification.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 /**
  * https://github.com/gouthampradhan/leetcode/blob/master/problems/src/string/TextJustification.java
+ * 题意：把一个集合的单词按照每行maxWidth个字符放，每行要两端对齐，如果空格不能均匀分布在所有间隔中，
+ * 那么左边的空格要多于右边的空格，最后一行靠左对齐，空格补全。
  *
  * @author genericyzh
  * @date 2017/10/12 23:32
@@ -18,13 +20,13 @@ public class _06_TextJustification {
         while (i < length) {
             int j = i + 1;
             int len = words[i].length();
-            while (j < length && len + 1 + words[j].length() < maxWidth) {
+            while (j < length && len + 1 + words[j].length() <= maxWidth) {
                 len += 1 + words[j++].length();
             }
 
             String line = words[i];
-            if (j == length) {
-                for (int k = i; k < j; k++) {
+            if (j == length) { // 最后一行
+                for (int k = i + 1; k < length; k++) {
                     line += " " + words[k];
                 }
                 while (line.length() < maxWidth) {
@@ -43,22 +45,24 @@ public class _06_TextJustification {
                         for (int l = 0; l < extraWhite / white; l++) {
                             line += " ";
                         }
-                        if (k - i < extraWhite % white) {
+                        if (k - i <= extraWhite % white) {
                             line += " ";
                         }
                         line += words[k];
                     }
                 }
-                result.add(line);
-                i = j;
             }
+            result.add(line);
+            i = j;
         }
         return result;
     }
 
     public static void main(String[] args) {
         _06_TextJustification textJustification = new _06_TextJustification();
-        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+//        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        String[] words = {"Here", "is", "an", "example", "of", "text", "justification."};
+//        String[] words = {""};
         List<String> strings = textJustification.fullJustify(words, 16);
         System.out.println(strings);
     }
