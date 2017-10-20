35fe5a2b93a8df2d24f1dcb18634a361ce5bbe3c
genericyzh <yaozihuan@163.com>
2017-10-20 23:55:38 +0800

    71. Simplify Path  简化路径

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/string/_12_SimplifyPath.java
@@ -0,0 +1,10 @@
+package com.genericyzh.algoDS.string;
+
+/**
+ * https://leetcode.com/problems/simplify-path/description/
+ *
+ * @author genericyzh
+ * @date 2017/10/20 23:54
+ */
+public class _12_SimplifyPath {
+}
