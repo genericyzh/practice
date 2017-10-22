bf3c15b96b92b3ed61ba2018cdf471a2435bd4a4
genericyzh <yaozihuan@163.com>
2017-10-22 23:52:50 +0800

    567. Permutation in String  挖坑

---
+++ leetcode/src/main/java/com/genericyzh/algoDS/string/_13_PermutationInString.java
@@ -0,0 +1,43 @@
+package com.genericyzh.algoDS.string;
+
+/**
+ * https://leetcode.com/problems/permutation-in-string/description/
+ *
+ * @author genericyzh
+ * @date 2017/10/22 23:27
+ */
+public class _13_PermutationInString {
+
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 > len2) return false;
+
+        int[] count = new int[26];
+        for (int i = 0; i < len1; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (allZero(count)) return true;
+
+        for (int i = len1; i < len2; i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - len1) - 'a']++;
+            if (allZero(count)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean allZero(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        _13_PermutationInString permutationInString = new _13_PermutationInString();
+
+    }
+
+}
