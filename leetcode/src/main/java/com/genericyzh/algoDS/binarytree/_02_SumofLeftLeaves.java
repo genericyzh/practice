44df45daaf85f442abd8734e336ed0b72e12aa87
genericyzh <yaozihuan@163.com>
2018-02-23 21:49:14 +0800

    【Update】_02_SumofLeftLeaves

--- leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_02_SumofLeftLeaves.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_02_SumofLeftLeaves.java
@@ -7,6 +7,13 @@ import java.util.Stack;
 /**
  * https://leetcode.com/problems/sum-of-left-leaves/description/
  * 给出一个二叉树，输出"左叶子节点"之和
+ * ** 3
+ * * / \
+ * *9  20
+ * ** /  \
+ * * 15   7
+ * <p>
+ * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
  *
  * @author genericyzh
  * @date 2017/8/29 22:59
@@ -25,6 +32,7 @@ public class _02_SumofLeftLeaves {
         }
         int sum = 0;
         if (root.left != null) {
+            // 判断左叶子节点
             if (root.left.left == null && root.left.right == null) {
                 sum += root.left.val;
             } else {
@@ -38,7 +46,31 @@ public class _02_SumofLeftLeaves {
     }
 
     /**
-     * 解法二：DFS
+     * 错误解法，左叶子节点判断错误
+     * root不知道是左节点还是右节点
+     *
+     * @param root
+     * @return
+     */
+    public int sumOfLeftLeaves4(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        if (root.left != null) {
+            sum += sumOfLeftLeaves(root.left);
+        }
+        if (root.right != null) {
+            sum += sumOfLeftLeaves(root.right);
+        }
+        return sum;
+    }
+
+    /**
+     * 解法二：BFS + 队列辅助
      *
      * @param root
      * @return
@@ -77,22 +109,26 @@ public class _02_SumofLeftLeaves {
      * @return
      */
     public int sumOfLeftLeaves3(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null) {
+            return 0;
+        }
         int ans = 0;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
 
         while (!stack.empty()) {
             TreeNode node = stack.pop();
             if (node.left != null) {
-                if (node.left.left == null && node.left.right == null)
+                if (node.left.left == null && node.left.right == null) {
                     ans += node.left.val;
-                else
+                } else {
                     stack.push(node.left);
+                }
             }
             if (node.right != null) {
-                if (node.right.left != null || node.right.right != null)
+                if (node.right.left != null || node.right.right != null) {
                     stack.push(node.right);
+                }
             }
         }
         return ans;
