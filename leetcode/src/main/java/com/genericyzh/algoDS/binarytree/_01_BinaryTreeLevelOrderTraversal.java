dd41cda48c034a8d85b6276ddebdbc191c280899
genericyzh <yaozihuan@163.com>
2018-02-23 14:51:46 +0800

    【Update】_01_BinaryTreeLevelOrderTraversal.java 统一阿里代码规范

--- leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_01_BinaryTreeLevelOrderTraversal.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_01_BinaryTreeLevelOrderTraversal.java
@@ -17,7 +17,8 @@ public class _01_BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) {
-            return result; // 这里应该返回[]，而不是null
+            // 这里应该返回[]，而不是null
+            return result;
         }
 
         Queue<TreeNode> queue = new LinkedList<>();
@@ -29,11 +30,23 @@ public class _01_BinaryTreeLevelOrderTraversal {
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 list.add(node.val);
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
             }
             result.add(list);
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+//        TreeNode node = TreeNode.mkTree("[3,9,20,null,null,15,7]");
+        TreeNode node = TreeNode.mkTree("[1,1,2,1,null,2,null,null,null,null,2]");
+        _01_BinaryTreeLevelOrderTraversal binaryTreeLevelOrderTraversal = new _01_BinaryTreeLevelOrderTraversal();
+        List<List<Integer>> list = binaryTreeLevelOrderTraversal.levelOrder(node);
+        list.forEach(System.out::println);
     }
 }
