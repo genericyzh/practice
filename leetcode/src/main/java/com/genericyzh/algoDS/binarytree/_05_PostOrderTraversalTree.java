2a013a8acc5ffdfbe85e73bd488f8154059c909c
genericyzh <yaozihuan@163.com>
2018-02-24 22:21:47 +0800

    【Add】_05_PostOrderTraversalTree 修改注释

--- leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_05_PostOrderTraversalTree.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_05_PostOrderTraversalTree.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 /**
  * https://leetcode.com/problems/binary-tree-postorder-traversal
- * 给出一个二叉树，返回后序遍历的节点val数组
+ * 给出一个二叉树，返回后序遍历(【左右根】)的节点val数组
  *
  * @author genericyzh
  * @date 2017/9/2 18:30
