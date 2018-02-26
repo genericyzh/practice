2ec143cc4121518569f529bdb389d737b1117465
genericyzh <yaozihuan@163.com>
2018-02-26 16:32:06 +0800

    【Update】_09_BinaryTreeInorderTraversal 修改注释

--- leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_09_BinaryTreeInorderTraversal.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_09_BinaryTreeInorderTraversal.java
@@ -13,8 +13,14 @@ import java.util.Stack;
  */
 public class _09_BinaryTreeInorderTraversal {
 
+    /**
+     * 非递归，用栈辅助
+     *
+     * @param root
+     * @return
+     */
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
 
         Stack<TreeNode> stack = new Stack<>();
         TreeNode cur = root;
@@ -33,26 +39,18 @@ public class _09_BinaryTreeInorderTraversal {
     }
 
 
+    /**
+     * 递归
+     *
+     * @param root
+     * @return
+     */
     public List<Integer> inorderTraversal2(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-//        inOrder(root, list);
-        inOrderIterative(root, list);
+        inOrder(root, list);
         return list;
     }
 
-    private void inOrderIterative(TreeNode x, List<Integer> list) {
-        Stack<TreeNode> stack = new Stack<>();
-
-        while (x != null || !stack.isEmpty()) {
-            while (x != null) {
-                stack.add(x);
-                x = x.left;
-            }
-            x = stack.pop();
-            list.add(x.val);
-            x = x.right;
-        }
-    }
 
     void inOrder(TreeNode x, List<Integer> list) {
         if (x == null) return;
