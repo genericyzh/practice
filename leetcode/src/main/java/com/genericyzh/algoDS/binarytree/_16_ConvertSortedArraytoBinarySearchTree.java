0a673d69964db8b659d403132b6ba565141a6471
genericyzh <yaozihuan@163.com>
2018-02-27 16:05:40 +0800

    【Update】_16_ConvertSortedArraytoBinarySearchTree  修改注释

--- leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_16_ConvertSortedArraytoBinarySearchTree.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_16_ConvertSortedArraytoBinarySearchTree.java
@@ -3,7 +3,7 @@ package com.genericyzh.algoDS.binarytree;
 /**
  * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
  * 给出一个升序数组，将其转换成一个尽量平衡的二叉树
- * 注意边界，当nums长度为2时，l = m = 0，下次的l = 0，r = -1(m-1)，所以种植条件为:l > r -> return null
+ * 注意边界，当nums长度为2时，l = m = 0，下次的l = 0，r = -1(m-1)，所以终止条件为:l > r -> return null
  * 当nums.length = 3时，第二次递归，左节点：l = r = 0，右节点：l = r = 2,所以l > r -> return null没问题
  *
  * @author genericyzh
@@ -20,6 +20,7 @@ public class _16_ConvertSortedArraytoBinarySearchTree {
     }
 
     private TreeNode buildBST(int[] nums, int l, int r) {
+        // 注意这里
         if (l > r) {
             return null;
         }
