bb7517e7b13d09dd5a3ae9382f19388deb3e77b0
genericyzh <yaozihuan@163.com>
2018-02-27 11:26:31 +0800

    【Update】_14_ConvertSortedListtoBinarySearchTree

--- leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_14_ConvertSortedListtoBinarySearchTree.java
+++ leetcode/src/main/java/com/genericyzh/algoDS/binarytree/_14_ConvertSortedListtoBinarySearchTree.java
@@ -23,6 +23,13 @@ public class _14_ConvertSortedListtoBinarySearchTree {
         return build(head, null);
     }
 
+    /**
+     * 注：最后的元素不包含在内（end可能为null，可能不为null）
+     * 实际长度可理解为end - start，比如：1, 2, 3，那么3在以下代码中可以算是null（fast != end && fast.next != end）
+     * @param start
+     * @param end
+     * @return
+     */
     private TreeNode build(ListNode start, ListNode end) {
         if (start == end) {
             return null;
@@ -38,5 +45,46 @@ public class _14_ConvertSortedListtoBinarySearchTree {
         treeNode.left = build(start, slow);
         treeNode.right = build(slow.next, end);
         return treeNode;
+    }
+
+    /**
+     * 错误示范：
+     * 以下算法及时断开后，拿不到正确的mid
+     * @param head
+     * @return
+     */
+    public TreeNode sortedListToBST2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next; // 当只有2个节点的时候，slow应该是第一个
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode treeNode = new TreeNode(slow.val);
+        ListNode temp = slow.next;
+        slow.next = null;
+        treeNode.left = sortedListToBST2(head);
+        treeNode.right = sortedListToBST2(temp);
+        slow.next = temp;
+        return treeNode;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(-10);
+        ListNode listNode2 = new ListNode(-3);
+        ListNode listNode3 = new ListNode(0);
+        ListNode listNode4 = new ListNode(5);
+        ListNode listNode5 = new ListNode(9);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        _14_ConvertSortedListtoBinarySearchTree convertSortedListtoBinarySearchTree = new _14_ConvertSortedListtoBinarySearchTree();
+        TreeNode node = convertSortedListtoBinarySearchTree.sortedListToBST(listNode1);
     }
 }
