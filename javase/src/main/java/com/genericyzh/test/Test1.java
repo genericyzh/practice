139dc396c589c391015213d14e338e84853d7c76
genericyzh <yaozihuan@163.com>
2018-10-15 23:51:11 +0800

    【Add】测试修饰符

---
+++ javase/src/main/java/com/genericyzh/test/Test1.java
@@ -0,0 +1,15 @@
+package com.genericyzh.test;
+
+/**
+ * @author genericyzh
+ * @date 2018/3/27 18:47
+ */
+class Test1 {
+
+    public static int a;
+
+    public static void main(String args[]) {
+
+    }
+
+}
