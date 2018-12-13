201d3f8e6f3257f8ae2ca27821f1e405851cf30c
genericyzh <yaozihuan@163.com>
2018-12-13 23:37:54 +0800

    【Add】builder模式

---
+++ DesignPattern/src/main/java/com/genericyzh/creational/builder/Director.java
@@ -0,0 +1,40 @@
+package com.genericyzh.creational.builder;
+
+/**
+ * @author genericyzh
+ * @date 2018/12/13 23:24
+ * 指导者，指导使用生成器的接口来构建产品的对象
+ */
+public class Director {
+
+    /**
+     * 持有当前需要使用的生成器对象
+     */
+    private Builder builder;
+
+
+    /**
+     * 构造方法，传入生成器对象
+     *
+     * @param builder 生成器对象
+     */
+    public Director(Builder builder) {
+
+        this.builder = builder;
+
+    }
+
+
+    /**
+     * 示意方法，指导生成器构建最终的产品对象
+     */
+    public void construct() {
+
+        //通过使用生成器接口来构建最终的产品对象
+
+        builder.buildPart();
+
+    }
+
+}
+
---
+++ DesignPattern/src/main/java/com/genericyzh/creational/builder/Product.java
@@ -0,0 +1,9 @@
+package com.genericyzh.creational.builder;
+
+/**
+ * @author genericyzh
+ * @date 2018/12/13 23:24
+ * 被构建的产品对象的接口
+ */
+public interface Product {
+}
